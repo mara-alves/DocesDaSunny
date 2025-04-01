48bd4f37c0239928efc9386aa1795bd03afcf4ae
mara-alves <maraalves2001@gmail.com>
2025-04-01 12:18:12 +0100

    Add colors and start layout

---
+++ src/app/_components/layout/Sidebar.tsx
@@ -0,0 +1,12 @@
+export default function Sidebar() {
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="border-base-content bg-primary border-2 px-2 py-1">
+        Filtrar Receitas
+      </div>
+      <div className="border-base-content bg-primary border-2 px-2 py-1">
+        Conversor de Medidas
+      </div>
+    </div>
+  );
+}
