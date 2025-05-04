eaa2c8c9bf8bb2e6d522a9ec34187f73cfd37211
mara-alves <maraalves2001@gmail.com>
2025-05-04 16:50:48 +0100

    Animate dropdowns

--- src/app/_components/inputs/SelectMenu.tsx
+++ src/app/_components/inputs/SelectMenu.tsx
@@ -6,6 +6,7 @@ import {
 } from "@headlessui/react";
 import { ChevronDown } from "lucide-react";
 import type { ReactNode } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 type SelectOption = { value: string; label: string; icon?: ReactNode };
 
@@ -19,31 +20,44 @@ export default function SelectMenu({
   options: SelectOption[];
 }) {
   return (
-    <Listbox value={value} onChange={setValue}>
-      <ListboxButton
-        className={
-          "bg-base border-base-content flex flex-row gap-1.5 border-2 px-2 py-1"
-        }
-      >
-        {value?.icon}
-        {value?.label}
-        <ChevronDown className="ml-auto" />
-      </ListboxButton>
-      <ListboxOptions
-        anchor="bottom"
-        className="bg-base text-base-content z-10 !max-h-48 w-(--button-width) overflow-y-auto border-2 shadow-lg"
-      >
-        {options.map((option, idx) => (
-          <ListboxOption
-            key={idx}
-            value={option}
-            className="data-focus:bg-primary flex cursor-pointer flex-row gap-1.5 px-2 py-1"
+    <Listbox value={value} onChange={setValue} as={"div"} className={"w-full"}>
+      {({ open }) => (
+        <>
+          <ListboxButton
+            className={
+              "bg-base border-base-content flex w-full flex-row gap-1.5 border-2 px-2 py-1"
+            }
           >
-            {option.icon}
-            {option.label}
-          </ListboxOption>
-        ))}
-      </ListboxOptions>
+            {value?.icon}
+            {value?.label}
+            <ChevronDown className="ml-auto" />
+          </ListboxButton>
+          <AnimatePresence>
+            {open && (
+              <ListboxOptions
+                static
+                as={motion.div}
+                initial={{ opacity: 0, height: 0 }}
+                animate={{ opacity: 1, height: "auto" }}
+                exit={{ opacity: 0, height: 0 }}
+                anchor="bottom"
+                className="bg-base text-base-content z-10 !max-h-48 w-(--button-width) origin-top overflow-y-auto border-2 shadow-lg"
+              >
+                {options.map((option, idx) => (
+                  <ListboxOption
+                    key={idx}
+                    value={option}
+                    className="data-focus:bg-primary flex cursor-pointer flex-row gap-1.5 px-2 py-1"
+                  >
+                    {option.icon}
+                    {option.label}
+                  </ListboxOption>
+                ))}
+              </ListboxOptions>
+            )}
+          </AnimatePresence>
+        </>
+      )}
     </Listbox>
   );
 }
--- src/app/_components/recipes/editMode/IngredientSelector.tsx
+++ src/app/_components/recipes/editMode/IngredientSelector.tsx
@@ -8,6 +8,7 @@ import {
 import { useState } from "react";
 import type { Ingredient } from "@prisma/client";
 import type { FrontendSectionIngredient } from "~/server/api/routers/recipe";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function IngredientSelector({
   value,
@@ -34,6 +35,7 @@ export default function IngredientSelector({
 
   return (
     <Combobox
+      as={"div"}
       immediate
       value={value}
       onChange={(e) => {
@@ -44,34 +46,50 @@ export default function IngredientSelector({
       }}
       onClose={() => setSearch("")}
     >
-      <ComboboxInput
-        onChange={(e) => setSearch(e.target.value)}
-        displayValue={(e) => (e as Ingredient)?.name}
-        className="border-base-content w-full border-2 px-2 py-1"
-      />
-      <ComboboxOptions
-        anchor="bottom start"
-        className="bg-base text-base-content z-10 !max-h-48 w-(--input-width) overflow-y-auto border-2 shadow-lg"
-      >
-        {filteredIngredients.map((e) => (
-          <ComboboxOption
-            key={e.name}
-            value={e}
-            className="data-focus:bg-primary cursor-pointer px-2 py-1"
-          >
-            {e.name}
-          </ComboboxOption>
-        ))}
-        {search.length > 2 &&
-          !filteredIngredients.find((e) => e.name === search) && (
-            <ComboboxOption
-              value={{ id: null, name: search }}
-              className="data-focus:bg-primary cursor-pointer px-2 py-1"
-            >
-              Criar <span className="font-semibold">&quot;{search}&quot;</span>
-            </ComboboxOption>
-          )}
-      </ComboboxOptions>
+      {({ open }) => (
+        <>
+          <ComboboxInput
+            onChange={(e) => setSearch(e.target.value)}
+            displayValue={(e) => (e as Ingredient)?.name}
+            className="border-base-content w-full border-2 px-2 py-1"
+          />
+          <AnimatePresence>
+            {open && (
+              <ComboboxOptions
+                static
+                as={motion.div}
+                initial={{ opacity: 0, height: 0 }}
+                animate={{ opacity: 1, height: "auto" }}
+                exit={{ opacity: 0, height: 0 }}
+                anchor="bottom"
+                className="bg-base text-base-content z-10 !max-h-48 w-(--input-width) overflow-y-auto border-2 shadow-lg"
+              >
+                {filteredIngredients.map((e) => (
+                  <ComboboxOption
+                    key={e.name}
+                    value={e}
+                    className="data-focus:bg-primary cursor-pointer px-2 py-1"
+                  >
+                    {e.name}
+                  </ComboboxOption>
+                ))}
+                {search.length > 2 &&
+                  !filteredIngredients.find((e) => e.name === search) && (
+                    <ComboboxOption
+                      value={{ id: null, name: search }}
+                      className="data-focus:bg-primary cursor-pointer px-2 py-1"
+                    >
+                      Criar{" "}
+                      <span className="font-semibold">
+                        &quot;{search}&quot;
+                      </span>
+                    </ComboboxOption>
+                  )}
+              </ComboboxOptions>
+            )}
+          </AnimatePresence>
+        </>
+      )}
     </Combobox>
   );
 }
